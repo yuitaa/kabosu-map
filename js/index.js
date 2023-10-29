dcd4e93776747ba0402fb4d862597f796b894f84
yuitaa <95457974+yuitaa@users.noreply.github.com>
2023-10-29 17:33:51 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -62,7 +62,7 @@ window.onload = () => {
   for (let x = -10; x <= 9; x++) {
     for (let y = -9; y <= 10; y++) {
       L.imageOverlay(
-        `./../img/tiles/zzzzzz_${x * 64}_${
+        `./img/tiles/zzzzzz_${x * 64}_${
           y * 64
         }.jpg`,
         [
